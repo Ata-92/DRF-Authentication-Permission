012393b061b328e5f4c02668166a5543d590e4f9
Ata-92 <atakal92@gmail.com>
2021-08-12 22:17:27 +0300

    Add an app

--- drf_auth/settings.py
+++ drf_auth/settings.py
@@ -43,9 +43,10 @@ INSTALLED_APPS = [
     # 3rd party apps,
     'rest_framework',
     'corsheaders',
-
+    "rest_framework.authtoken",
     # my apps
     'student_api',
+    'user_api'
 
 ]
 
@@ -140,3 +141,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+    ]
+}
