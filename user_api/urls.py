c5d2d597c4e1bd259f7b7f7f84f8faf6a789d1de
Ata-92 <atakal92@gmail.com>
2021-08-13 00:07:39 +0300

    Add permissions

--- user_api/urls.py
+++ user_api/urls.py
@@ -1,6 +1,9 @@
+from user_api.views import logout_view, register_view
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
 
 urlpatterns = [
     path("login/", obtain_auth_token, name="login"),
+    path("register/", register_view, name="register"),
+    path("logout/", logout_view, name="logout"),
 ]
--- user_api/views.py
+++ user_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
+from . import models
 
 # Create your views here.
 
@@ -9,11 +10,22 @@ from rest_framework.authtoken.models import Token
 def register_view(request):
     if request.method == "POST":
         serializer =  RegistrationSerializer(data=request.data)
+        data = {}
         if serializer.is_valid():
             account = serializer.save()
+            # account.set_password(account.password)
             token, _ = Token.objects.get_or_create(user=account)
-            data = serializer.data
+            # data = serializer.data
             data["token"] = token.key
         else:
             data = serializer.errors
+        return Response(data)
+
+@api_view(["POST"])
+def logout_view(request):
+    if request.method == "POST":
+        request.user.auth_token.delete()
+        data = {
+            "message": "logout, token deleted"
+        }
         return Response(data)
