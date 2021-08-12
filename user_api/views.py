d88cfb43f820f42440bc6f4c23632f28f51a3c98
Ata-92 <atakal92@gmail.com>
2021-08-12 22:57:21 +0300

    Add register serializer and its function view

--- user_api/views.py
+++ user_api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
+
+@api_view(["POST"])
+def register_view(request):
+    if request.method == "POST":
+        serializer =  RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            account = serializer.save()
+            token, _ = Token.objects.get_or_create(user=account)
+            data = serializer.data
+            data["token"] = token.key
+        else:
+            data = serializer.errors
+        return Response(data)
